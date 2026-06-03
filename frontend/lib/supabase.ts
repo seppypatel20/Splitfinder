1f0a777103f929f1d53d65478f9a301787e1fbbc
seppypatel20 <seppypatel20@gmail.com>
2026-06-03 13:02:46 +1000

    Update supabase.ts

--- frontend/lib/supabase.ts
+++ frontend/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://YOUR_PROJECT_ID.supabase.co";
-const supabaseAnonKey = "YOUR_ANON_KEY";
+const supabaseUrl = "https://omovspdppacckrdtldpc.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tb3ZzcGRwcGFjY2tyZHRsZHBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0NDIxOTEsImV4cCI6MjA5NjAxODE5MX0.Yf4lz5023OtR-LcbZIi7UNcWwTuRHqJvE73g8l1irow";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
